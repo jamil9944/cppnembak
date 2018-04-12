9efac680a27355e4ef2e4678c75b30b69e76312c
Achmad Choirun Nasrukhin Wakhid <azvr44@gmail.com>
2018-04-12 21:00:06 +0700

    Create script.cpp

---
+++ script.cpp
@@ -0,0 +1,54 @@
+#include<iostream> 
+#include<fstream> 
+#include<windows.h>
+//Creator : Anazvr
+//Garuda Security Hacker
+using namespace std;
+int main()
+{ 
+int y;
+system("color 0b");
+ofstream fileteks; 
+//ISI FILE
+fileteks.open("C:/My Heart.txt");//nama file dan tempat 
+fileteks << "()___())"<< endl; //isi
+fileteks << "(= ‘.’=)"<< endl; //isi
+fileteks << "(”)__(”)"<< endl; //isi
+fileteks << "I LOVE U"<< endl; //isi
+fileteks << "-Anas"<< endl;    //isi
+fileteks.close();
+cout << "                   |============================================|\n"; 
+cout << "                   |                                            |\n"; 
+cout << "                   |======+++++GARUDA SECURITY HACKER+++++======|\n";
+cout << "                   |           ________ANAZVR________           |\n"; 
+cout << "                   |                                            |\n"; 
+cout << "                   |============================================|\n"; 
+//LOADING
+
+ cout<<"\n\n\n\t\t\t\t    Sabar ^_^\n\n";
+ char a=177, b=219;
+ cout<<"\t\t\t\t";
+ for (int i=0;i<=15;i++)
+ cout<<a;
+ cout<<"\r";
+ cout<<"\t\t\t\t";
+ for (int i=0;i<=15;i++)
+ {
+  cout<<b;
+  for (int j=0;j<=1e8;j++); 
+ }
+
+cout<<"\n\n\n\t\t\t            SELESAI\n\n\t\t\t";
+cout << "\n";
+cout << "          	     ___________________________________________\n";
+cout << "                      Tolong Baca File Yang Ada Di Local Disk C\n";
+cout << "                                 nama filenya My Heart.txt\n";
+cout << "\n";
+cout << "                                                     --Anazvr\n";
+cout << "          	     ___________________________________________\n\n\n\n";
+cout << "Jangan lupa dibaca >.<\n";
+cout << "Klik Enter 2 kali untuk keluar";
+
+cin.get();
+return 0;
+} 
